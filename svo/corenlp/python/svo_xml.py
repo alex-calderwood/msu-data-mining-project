d17c39993017083b6f599fff0125464f0a521b1f
Alex Calderwood <Alex Calderwood>
2016-12-04 20:08:06 -0700

    Created freqency counts of text occuring in svo triples.

--- svo/corenlp/python/svo_xml.py
+++ svo/corenlp/python/svo_xml.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
-
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+stopwords = set(stopwords.words('english'))
 
 def getXMLRoot(xmlFileName):
     xmlTree = ET.parse(xmlFileName)
@@ -25,13 +27,29 @@ def getSPOTuples(xmlRoot):
 
     return subjects, predicates, objects
 
+def listTokenFrequencies(tokenList):
+    if tokenList is None:
+        return
 
-def occurances(entityList):
+    freqDict = {}
+
+    for token in tokenList:
+        if freqDict.get(token) is None:
+            freqDict[token] = 1
+        else:
+            freqDict[token] += 1
+
+    print(freqDict)
+
+
+
+
+def occurances(tokenList):
     trumpTripleIndices = []
     clintonTripleIndices = []
 
-    for i in range(len(entityList)):
-        text = entityList[i]
+    for i in range(len(tokenList)):
+        text = tokenList[i]
         if 'Trump' in text:
             trumpTripleIndices.append(i)
         elif 'Clinton' in text:
@@ -40,6 +58,15 @@ def occurances(entityList):
     print("Trump: ", len(trumpTripleIndices), "Clinton:", len(clintonTripleIndices))
     return trumpTripleIndices, clintonTripleIndices
 
+def getTokensFromList(indicesToExtract, textList):
+    tokens = []
+    for i in indicesToExtract:
+        for token in word_tokenize(textList[i]):
+            token = token.lower()
+            if token not in stopwords:
+                tokens.append(token)
+    return tokens
+
 
 if __name__ == '__main__':
     fileName = "test.xml"
@@ -47,7 +74,7 @@ if __name__ == '__main__':
     subjects, predicates, objects = getSPOTuples(root)
 
     print(subjects)
-    subTrumps, subClintons = occurances(subjects)
+    indicesOfTrumpSubs, indicesOfClintonSubs = occurances(subjects)
 
     print(predicates)
     occurances(predicates)
@@ -55,10 +82,39 @@ if __name__ == '__main__':
     print(objects)
     occurances(objects)
 
-    print('\nTrump Triples:')
-    for tripleIndex in subTrumps:
-        print(subjects[tripleIndex], predicates[tripleIndex], objects[tripleIndex])
 
-    print("\nClinton Triples:")
-    for tripleIndex in subClintons:
-        print(subjects[tripleIndex], predicates[tripleIndex], objects[tripleIndex])
+    trumpPreds = []
+    trumpObjs = []
+    print('\nTrump Triples:')
+    for tripleIndex in indicesOfTrumpSubs:
+        print(subjects[tripleIndex], '|', predicates[tripleIndex], '|', objects[tripleIndex])
+        trumpPreds.append(predicates[tripleIndex])
+        trumpObjs.append(predicates[tripleIndex])
+
+    # Print out the frequencies of tokens that occur as predicates when Trump occurs in the subject
+    print('Trump Predicate Frequencies:')
+    print(listTokenFrequencies(getTokensFromList(indicesOfTrumpSubs, predicates)))
+    # Print out the frequencies of tokens that occur as objects when Trump occurs in the subject
+    print('Trump Object Frequencies:')
+    print(listTokenFrequencies(getTokensFromList(indicesOfTrumpSubs, objects)))
+    
+    
+    # Do the same thing for Clinton TODO: Extract this to a function
+    clintonPreds = []
+    clintonObjs = []
+    print('\nClinton Triples:')
+    for tripleIndex in indicesOfClintonSubs:
+        print(subjects[tripleIndex], '|', predicates[tripleIndex], '|', objects[tripleIndex])
+        clintonPreds.append(predicates[tripleIndex])
+        clintonObjs.append(predicates[tripleIndex])
+
+    # Print out the frequencies of tokens that occur as predicates when Clinton occurs in the subject
+    print('Clinton Predicate Frequencies:')
+    print(listTokenFrequencies(getTokensFromList(indicesOfClintonSubs, predicates)))
+    # Print out the frequencies of tokens that occur as objects when Clinton occurs in the subject
+    print('Clinton Object Frequencies:')
+    print(listTokenFrequencies(getTokensFromList(indicesOfClintonSubs, objects)))
+
+    # print("\nClinton Triples:")
+    # for tripleIndex in indicesofClintonSubs:
+    #     print(subjects[tripleIndex],'|', predicates[tripleIndex],'|', objects[tripleIndex])
