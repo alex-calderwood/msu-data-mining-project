689c63eef99e3a687d5142bd503621e875943afb
fippy24 <connor.trumpet@gmail.com>
2016-12-04 22:39:08 -0700

    che che che changes

--- Readability/DCReadability.py
+++ Readability/DCReadability.py
@@ -10,20 +10,29 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 def grade(doc):
+    stemmer = PorterStemmer()
     commonWordsFile = open("Readability/The_Dale-Chall_Word_List.txt", "r")
     cw= commonWordsFile.readlines()
+    i=0
+    dict={}
+    #cw=unicode(cw,'utf-8')
+    for w in cw:
+        ws=stemmer.stem(unicode(w,'utf-8'))
+        dict[ws]=1
 
     sentences = sent_tokenize(doc)
     words = word_tokenize(doc)
     ASL = len(words)/len(sentences)-1 #-1 for each period
     i=0
-    stemmer = PorterStemmer()
+    
     for word in words:
         rootWord = stemmer.stem(word)
-        if any(rootWord in w for w in cw):
+        if dict.has_key(rootWord):
             i=i+1
     DS = 1-i/(len(words)-len(sentences))
-    RGS = (0.1579 * DS*100) + (0.0496 * ASL) + 3.6365
+    RGS = (0.1579 * DS) + (0.0496 * ASL)
+    if DS>.05:
+        RGS=RGS + 3.6365
     print "reading grade is:",RGS
 
     commonWordsFile.close()
